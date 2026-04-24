cc9e7ffb463c5510211dc8e51cd2ddfe704a2da8
Shanbel Dires <shanbeldires7@gmail.com>
2026-04-24 20:12:00 +0300

    adding deployed link in API path

--- Frontend/src/lib/api.jsx
+++ Frontend/src/lib/api.jsx
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api/v1'
+const API_BASE_URL = 'https://my-portfolio-1-uev3.onrender.com/api/v1'
 
 export const contactAPI = {
   submitContact: async (formData) => {
